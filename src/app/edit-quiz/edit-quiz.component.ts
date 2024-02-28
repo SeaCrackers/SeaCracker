f52f7c53eaac82585df859cc0ac45d6eb2a3649c
Shanshe <Shanshe.gundishvili@eduvaud.ch>
2024-02-28 12:28:11 +0100

    feat : add edit quiz component and route

---
+++ src/app/edit-quiz/edit-quiz.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-quiz',
+  standalone: true,
+  imports: [],
+  templateUrl: './edit-quiz.component.html',
+  styleUrl: './edit-quiz.component.scss'
+})
+export class EditQuizComponent {
+
+}
