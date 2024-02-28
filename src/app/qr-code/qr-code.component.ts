9741181fda0139a42397e128c43730786326d845
Cyprien Jaquier <61788240+Cyprien-png@users.noreply.github.com>
2024-02-28 12:02:33 +0100

    add: html view of the QR

--- src/app/qr-code/qr-code.component.ts
+++ src/app/qr-code/qr-code.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { QRCodeModule } from 'angularx-qrcode';
 
 @Component({
   selector: 'app-qr-code',
   standalone: true,
-  imports: [],
+  imports: [QRCodeModule],
   templateUrl: './qr-code.component.html',
   styleUrl: './qr-code.component.scss'
 })
