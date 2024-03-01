868a476f5dd62dc732f2fddc31d328d4e2faf820
Cyprien <61788240+Cyprien-png@users.noreply.github.com>
2024-03-01 17:56:27 +0100

    add: route to the 'joining a room' page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { RoomPlayerPageComponent } from './room-player-page/room-player-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'room', component: RoomPlayerPageComponent }
+];
