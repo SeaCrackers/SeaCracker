385dd5488deae793ee52a34b38221cbf03f6ea7f
Cyprien <61788240+Cyprien-png@users.noreply.github.com>
2024-03-02 17:19:19 +0100

    add: redirection to the desired room

--- src/app/room-code-player-page/room-code-player-page.component.ts
+++ src/app/room-code-player-page/room-code-player-page.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-room-code-player-page',
@@ -11,4 +11,10 @@ import { FormsModule } from '@angular/forms';
 })
 export class RoomCodePlayerPageComponent {
   roomID: string | null = null;
+
+  constructor(private router: Router) { }
+
+  joinRoom() {
+    this.router.navigate(['/room/', this.roomID]);
+  }
 }
