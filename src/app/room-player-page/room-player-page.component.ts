dabeffd4424d0e31e9716a8e9e30ecd0e4e16e2b
Cyprien <61788240+Cyprien-png@users.noreply.github.com>
2024-03-02 16:41:32 +0100

    add: form to pick a nickname on players room

--- src/app/room-player-page/room-player-page.component.ts
+++ src/app/room-player-page/room-player-page.component.ts
@@ -1,18 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 
 @Component({
     selector: 'app-room-player-page',
     standalone: true,
-    imports: [],
+    imports: [FormsModule],
     templateUrl: './room-player-page.component.html',
     styleUrl: './room-player-page.component.scss'
 })
 export class RoomPlayerPageComponent {
 
     roomID: string | null;
-    playerNickname: string | null = null;
+    playerNickName: string | null = null;
+
+    setPlayerNickName(nickname: string | null) {
+        this.playerNickName = nickname;
+    }
 
     constructor(private _Activatedroute: ActivatedRoute) {
         this.roomID = this._Activatedroute.snapshot.paramMap.get("id");
