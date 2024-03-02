48c469b61f998bd643958bec9bff034641b2ee68
Cyprien <61788240+Cyprien-png@users.noreply.github.com>
2024-03-02 16:59:20 +0100

    add: page to join a room by code

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { RoomPlayerPageComponent } from './room-player-page/room-player-page.component';
+import { RoomCodePlayerPageComponent } from './room-code-player-page/room-code-player-page.component';
 
 export const routes: Routes = [
+    { path: 'room', component: RoomCodePlayerPageComponent },
     { path: 'room/:id', component: RoomPlayerPageComponent }
 ];
---
+++ src/app/room-code-player-page/room-code-player-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-room-code-player-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './room-code-player-page.component.html',
+  styleUrl: './room-code-player-page.component.scss'
+})
+export class RoomCodePlayerPageComponent {
+
+}
