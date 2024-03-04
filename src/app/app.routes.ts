b4d7ab7e84975634d13c3386ed70efdc6d9ec275
EliottJaquierEdu <eliott.jaquier@eduvaud.ch>
2024-03-04 11:37:14 +0100

    add route for host and rename play

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { RoomPlayerPageComponent } from './room-player-page/room-player-page.component';
 import { RoomCodePlayerPageComponent } from './room-code-player-page/room-code-player-page.component';
+import {HostPageComponent} from "./host-page/host-page.component";
 
 export const routes: Routes = [
-    { path: 'room', component: RoomCodePlayerPageComponent },
-    { path: 'room/:id', component: RoomPlayerPageComponent }
+    { path: 'play', component: RoomCodePlayerPageComponent },
+    { path: 'play/:id', component: RoomPlayerPageComponent },
+    { path: 'host/:quiz', component: HostPageComponent }
 ];
--- src/app/host-page/host-page.component.ts
+++ src/app/host-page/host-page.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import {RoomCommunicationsService} from "../services/communications/room-communications.service";
+import {RandomGenerator} from "../utils/random-generator";
 @Component({
   selector: 'app-host-page',
   standalone: true,
@@ -8,14 +9,15 @@ import {RoomCommunicationsService} from "../services/communications/room-communi
   styleUrl: './host-page.component.scss'
 })
 export class HostPageComponent {
+  public roomCode = RandomGenerator.generateString(4);
   constructor(private room:RoomCommunicationsService) {
-    room.joinRoom("AAAA")
+    room.joinRoom(this.roomCode)
     room.onEventReceived<any>().subscribe((value)=>{
       console.log("Received message : "+value)
     });
   }
 
   sendMessage() {
-    this.room.sendEventTo("AAAA","Mouahaha!")
+    this.room.sendEventTo(this.roomCode,"Mouahaha!")
   }
 }
