732133cc47d35a69204e1fd53f0e0b557b90b7f5
Philippe Moeckli <135646622+LeZderich@users.noreply.github.com>
2024-03-04 20:40:17 +0100

    Create the homepage component
    Using bootstrap

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,39 @@
-import { Component } from '@angular/core';
-
+import {Component, OnInit} from '@angular/core';
+import { Quiz } from "../models/quiz.model";
+import { QuizService } from "../services/quiz.service"
+import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-home',
-  standalone: true,
-  imports: [],
   templateUrl: './home.component.html',
+  standalone: true,
+  imports: [CommonModule],
   styleUrl: './home.component.scss'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
+
+  title = 'Home Page';
+  constructor(private quizService: QuizService, private router: Router) { }
+
+  public quizzes: Quiz[] = [];
+
+  ngOnInit(): Promise<void> {
+    this.quizzes = this.quizService.getQuizzes();
+    return Promise.resolve();
+  }
+  public deleteQuiz(quiz: Quiz) {
+    this.quizzes = this.quizzes.filter(q => q.id !== quiz.id);
+  }
+
+  public editQuiz(quiz: Quiz) {
+    this.router.navigate(['/quiz', quiz.id, 'edit']);
+  }
+  public playQuiz(quiz: Quiz) {
+    console.log(quiz);
+  }
+
+  public addQuiz() {
+    //
+  }
 
 }
