c95d7b9fd7f579e6a6b2a14d5bb32272b78e77e9
Shanshe <Shanshe.gundishvili@eduvaud.ch>
2024-03-06 10:49:26 +0100

    feat : answer model

--- src/app/models/answer.model.ts
+++ src/app/models/answer.model.ts
@@ -1,4 +1,27 @@
 export class Answer {
-    constructor(public id: string, public answer: string, public correct: boolean) {
+    constructor(private id: string, private answer: string, private correct: boolean) {}
+
+    public getId(): string {
+      return this.id;
+    }
+
+    public getAnswer(): string {
+      return this.answer;
+    }
+
+    public getCorrect(): boolean {
+      return this.correct;
+    }
+
+    public setId(id: string) : void{
+      this.id = id;
+    }
+
+    public setAnswer(answer: string) : void{
+      this.answer = answer;
+    }
+
+    public setCorrect(correct: boolean) : void{
+      this.correct = correct;
     }
 }
