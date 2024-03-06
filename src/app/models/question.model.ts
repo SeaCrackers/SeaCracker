9bd21329477138515890de217a27c40339651aea
Shanshe <Shanshe.gundishvili@eduvaud.ch>
2024-03-06 10:49:17 +0100

    feat : question model

--- src/app/models/question.model.ts
+++ src/app/models/question.model.ts
@@ -1,5 +1,49 @@
 import {Answer} from "./answer.model";
 
 export class Question {
-    constructor(public id: string, public question: string, public timer : string, public answers : Answer[]) {  }
+  constructor(private id: number, private question: string, private timer : string, private answers: Answer[] = []){}
+
+  public getId(): number {
+    return this.id;
+  }
+
+  public getQuestion(): string {
+    return this.question;
+  }
+
+  public getTimer(): string {
+    return this.timer;
+  }
+
+  public getAnswers(): Answer[] {
+    return this.answers;
+  }
+
+  public setId(id: number) : void{
+    this.id = id;
+  }
+
+  public setQuestion(question: string) : void{
+    this.question = question;
+  }
+
+  public setTimer(timer: string) : void{
+    this.timer = timer;
+  }
+
+  public setAnswers(answers: Answer[]) : void{
+    this.answers = answers;
+  }
+
+  public addAnswer(answer: Answer) : void{
+    this.answers.push(answer);
+  }
+
+  public removeAnswer(answer: Answer) : void{
+    this.answers = this.answers.filter(a => a.getId() !== answer.getId());
+  }
+
+  public getCorrectAnswers(): Answer[] {
+    return this.answers.filter(a => a.getCorrect());
+  }
 }
