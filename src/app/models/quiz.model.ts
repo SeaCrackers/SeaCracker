73c6624df9ac3fed7abd8a940133259aca1d6f90
Shanshe <Shanshe.gundishvili@eduvaud.ch>
2024-03-06 10:49:10 +0100

    feat : quiz model

--- src/app/models/quiz.model.ts
+++ src/app/models/quiz.model.ts
@@ -1,6 +1,37 @@
 import {Question} from "./question.model";
 
 export class Quiz {
-  constructor(public id: number, public title: string, public questions : Question[]) {
+  constructor(private id: number, private title: string, private questions: Question[] = []) {}
+
+  public getId(): number {
+    return this.id;
+  }
+
+  public getTitle(): string {
+    return this.title;
+  }
+
+  public getQuestions(): Question[] {
+    return this.questions;
+  }
+
+  public setId(value: number) {
+    this.id = value;
+  }
+
+  set setTitle(value: string) {
+    this.title = value;
+  }
+
+  public setQuestions(value: Question[]) {
+    this.questions = value;
+  }
+
+  public addQuestion(question: Question) : void{
+    this.questions.push(question);
+  }
+
+  public removeQuestion(question: Question) : void{
+    this.questions = this.questions.filter(q => q.getId() !== question.getId());
   }
 }
