1aff22f8426bcc1db3565f6b4d5b4c28cb46fa8b
Philippe Moeckli <135646622+LeZderich@users.noreply.github.com>
2024-03-06 09:29:55 +0100

    Create the edit-quiz component

--- src/app/edit-quiz/edit-quiz.component.ts
+++ src/app/edit-quiz/edit-quiz.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import {ActivatedRoute, Router} from "@angular/router";
+import {QuizService} from "../services/quiz.service";
+import {Question} from "../models/question.model";
+import {Quiz} from "../models/quiz.model";
 
 @Component({
   selector: 'app-edit-quiz',
@@ -7,6 +11,18 @@ import { Component } from '@angular/core';
   templateUrl: './edit-quiz.component.html',
   styleUrl: './edit-quiz.component.scss'
 })
-export class EditQuizComponent {
+export class EditQuizComponent{
+  title = 'Edit Quiz';
+  quiz!: Quiz;
+
+  constructor(private quizService: QuizService, private router: Router, private route:ActivatedRoute) {
+    const id = this.route.snapshot.params["id"];
+    let potentialQuiz = this.quizService.getQuizById(id);
+    if (potentialQuiz == undefined) {
+      this.router.navigate(["/"])
+    }
+    this.quiz = potentialQuiz!;
+  }
+
 
 }
