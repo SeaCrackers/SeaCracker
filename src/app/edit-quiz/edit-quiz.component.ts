51f6004f00f7a96b4cab34cdb51e7e83151719b8
Philippe Moeckli <135646622+LeZderich@users.noreply.github.com>
2024-03-10 21:16:45 +0100

    Add the left side of the quiz editor

--- src/app/edit-quiz/edit-quiz.component.ts
+++ src/app/edit-quiz/edit-quiz.component.ts
@@ -3,6 +3,7 @@ import {ActivatedRoute, Router} from "@angular/router";
 import {QuizService} from "../services/quiz.service";
 import {Question} from "../models/question.model";
 import {Quiz} from "../models/quiz.model";
+import {Answer} from "../models/answer.model";
 
 @Component({
   selector: 'app-edit-quiz',
@@ -14,14 +15,42 @@ import {Quiz} from "../models/quiz.model";
 export class EditQuizComponent{
   title = 'Edit Quiz';
   quiz!: Quiz;
+  question!: Question;
 
   constructor(private quizService: QuizService, private router: Router, private route:ActivatedRoute) {
+
     const id = this.route.snapshot.params["id"];
     let potentialQuiz = this.quizService.getQuizById(id);
     if (potentialQuiz == undefined) {
       this.router.navigate(["/"])
     }
     this.quiz = potentialQuiz!;
+    if (this.quiz.getQuestions().length > 0) {
+      this.selectedQuestion(this.quiz.getQuestions()[0]);
+    }else {
+      this.question = this.addQuestion();
+      this.saveQuiz();
+    }
+  }
+  selectedQuestion(question: Question) {
+    return this.question = question;
+  }
+  deleteQuestion(id: number) {
+    const deleteQuestion = this.quiz.getQuestions().find(q=>q.getId()=== id);
+    if (deleteQuestion) {
+      this.quiz.removeQuestion(deleteQuestion);
+      this.saveQuiz();
+    }
+  }
+  addQuestion() : Question {
+    const newQuestion = new Question(this.quiz.getQuestions().length, "New Question", 20, [
+        new Answer('0', "New Answer 1", false),
+        new Answer('1', "New Answer 2", false),
+        new Answer('2', "New Answer 3", false),
+        new Answer('3', "New Answer 4", true)]);
+    this.quiz.addQuestion(newQuestion);
+    this.saveQuiz();
+    return newQuestion;
   }
 
 
