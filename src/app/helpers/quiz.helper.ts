6cecef448fc2ffc3badc2052c4256d8e6a383d94
Shanshe Gundishvili <ShansheGundishvili@eduvaud.ch>
2024-03-17 00:53:17 +0100

    chore : add actual class to helper

---
+++ src/app/helpers/quiz.helper.ts
@@ -0,0 +1,6 @@
+import {Quiz} from "../interfaces/quiz.interface";
+import {Question, Timers} from "../interfaces/question.interface";
+import {Answer} from "../interfaces/answer.interface";
+
+export class QuizHelper {
+}
