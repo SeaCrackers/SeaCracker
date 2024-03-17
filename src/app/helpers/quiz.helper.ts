fc3e3855c81fed5b1e03577126658411a5106719
Shanshe Gundishvili <ShansheGundishvili@eduvaud.ch>
2024-03-17 01:00:04 +0100

    feat : add save and load data from localstorage

--- src/app/helpers/quiz.helper.ts
+++ src/app/helpers/quiz.helper.ts
@@ -3,4 +3,35 @@ import {Question, Timers} from "../interfaces/question.interface";
 import {Answer} from "../interfaces/answer.interface";
 
 export class QuizHelper {
+  private quizzes: Quiz[];
+  constructor(){
+    this.quizzes = this.getQuizzesFromLocalStorage();
+  }
+
+  // SAVED DATA
+
+  getQuizzesFromLocalStorage(): Quiz[]{
+    if (!localStorage) throw new Error('Local storage is not supported');
+
+    let item : string | null = localStorage.getItem('quizzes');
+    if (item) {
+      try {
+        return JSON.parse(item) as Quiz[];
+      } catch (error) {
+        throw new Error('Failed to parse quizzes from local storage');
+      }
+    } else {
+      return [];
+    }
+  }
+
+  saveQuizzesToLocalStorage(): void{
+    if (!localStorage) throw new Error('Local storage is not supported');
+
+    try {
+      localStorage.setItem('quizzes', JSON.stringify(this.quizzes));
+    } catch (error) {
+      throw new Error('Failed to save quizzes to local storage');
+    }
+  }
 }
