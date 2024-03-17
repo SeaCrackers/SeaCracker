a4ac79130e4e9af2e26ab6e219bfd46d1374527c
eliott <eliott.jaquier@gmail.com>
2024-03-17 16:07:12 +0100

    fix : host page enums

--- src/app/host-page/host-page.component.ts
+++ src/app/host-page/host-page.component.ts
@@ -6,6 +6,7 @@ import { QrCodeComponent } from '../qr-code/qr-code.component';
 import {HostQuestionComponent} from "../host/host-question/host-question.component";
 import {HostPlayerlistComponent} from "../host/host-playerlist/host-playerlist.component";
 import {HostPodiumComponent} from "../host/host-podium/host-podium.component";
+import {GameStepType} from "../services/game/steps/game-step-type";
 @Component({
   selector: 'app-host-page',
   standalone: true,
@@ -17,4 +18,6 @@ export class HostPageComponent {
   constructor(public host:HostService) {
 
   }
+
+  protected readonly GameStepType = GameStepType;
 }
