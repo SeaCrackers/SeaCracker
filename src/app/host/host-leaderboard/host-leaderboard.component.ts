5cca6cf9b876d2951025f88074123fca745e0da3
eliott <eliott.jaquier@gmail.com>
2024-03-17 16:42:34 +0100

    feat : add host leaderboard component

---
+++ src/app/host/host-leaderboard/host-leaderboard.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-host-leaderboard',
+  standalone: true,
+  imports: [],
+  templateUrl: './host-leaderboard.component.html',
+  styleUrl: './host-leaderboard.component.scss'
+})
+export class HostLeaderboardComponent {
+
+}
