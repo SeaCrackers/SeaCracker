3ba4081117576e61ea90eed02c17a06085613d4f
eliott <eliott.jaquier@gmail.com>
2024-03-17 16:42:42 +0100

    feat : add host answer component

---
+++ src/app/host/host-answer/host-answer.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-host-answer',
+  standalone: true,
+  imports: [],
+  templateUrl: './host-answer.component.html',
+  styleUrl: './host-answer.component.scss'
+})
+export class HostAnswerComponent {
+
+}
