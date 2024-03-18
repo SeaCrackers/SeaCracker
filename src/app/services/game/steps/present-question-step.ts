1de7dfca242c8d5997c6414ea03bf7cbac4914f8
Cyprien <61788240+Cyprien-png@users.noreply.github.com>
2024-03-18 23:39:53 +0100

    feat: add getter for the timer

--- src/app/services/game/steps/present-question-step.ts
+++ src/app/services/game/steps/present-question-step.ts
@@ -3,9 +3,11 @@ import { AnswerStep } from "./answer-step";
 import { LeaderboardStep } from "./leaderboard-step";
 import { filter, firstValueFrom, map, merge, scan, Observable } from "rxjs";
 
-const TIME_TO_PRESENT_QUESTION = 3000;
+const TIME_TO_PRESENT_QUESTION = 10000;
 
 export class PresentQuestionStep extends GameStep {
+  private timeSpent: number = 0;
+
   goToNextStep(): GameStep {
     return new AnswerStep(this.gameState);
   }
@@ -16,10 +18,17 @@ export class PresentQuestionStep extends GameStep {
 
   override onIsReadyToMoveToNextStep(): Observable<void> {
     return new Observable((subscriber) => {
-      setTimeout(() => {
-        subscriber.next();
-      }, TIME_TO_PRESENT_QUESTION);
+      setInterval(() => {
+        this.timeSpent += 1;
+        if (this.timeSpent * 1000 >= TIME_TO_PRESENT_QUESTION) {
+          subscriber.next();
+        }
+      }, 1000);
     });
+  }
+
+  getRemainingTime(): number {
+    return TIME_TO_PRESENT_QUESTION/1000 - this.timeSpent;
   }
 
   override needManualInput(): boolean {
