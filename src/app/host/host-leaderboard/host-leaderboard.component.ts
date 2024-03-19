17d407f65db6a6b14892aa3157f09c61161f9c66
Cyprien <61788240+Cyprien-png@users.noreply.github.com>
2024-03-19 23:40:26 +0100

    fix: leaderboard

--- src/app/host/host-leaderboard/host-leaderboard.component.ts
+++ src/app/host/host-leaderboard/host-leaderboard.component.ts
@@ -1,9 +1,9 @@
-import {Component, Input} from '@angular/core';
-import {PlayerListStep} from "../../services/game/steps/player-list-step";
-import {LeaderboardStep} from "../../services/game/steps/leaderboard-step";
-import {HostComponent} from "../host-component";
-import {HostService} from "../../services/game/host.service";
-import {AnswerStep} from "../../services/game/steps/answer-step";
+import { Component, Input } from '@angular/core';
+import { PlayerListStep } from "../../services/game/steps/player-list-step";
+import { LeaderboardStep } from "../../services/game/steps/leaderboard-step";
+import { HostComponent } from "../host-component";
+import { HostService } from "../../services/game/host.service";
+import { AnswerStep } from "../../services/game/steps/answer-step";
 
 @Component({
   selector: 'app-host-leaderboard',
@@ -12,10 +12,18 @@ import {AnswerStep} from "../../services/game/steps/answer-step";
   templateUrl: './host-leaderboard.component.html',
   styleUrl: './host-leaderboard.component.scss'
 })
-export class HostLeaderboardComponent extends HostComponent{
+export class HostLeaderboardComponent extends HostComponent {
   private step: LeaderboardStep;
-  constructor(host:HostService) {
+  constructor(host: HostService) {
     super(host);
     this.step = host.getCurrentStep()() as LeaderboardStep;
+  }
+
+  getLeaderboard() {
+    const players = this.host.getCurrentStep()()!.getGameState().getPlayers()();
+    
+    return players.sort((a, b) => {
+      return b.score - a.score;
+    });
   }
 }
