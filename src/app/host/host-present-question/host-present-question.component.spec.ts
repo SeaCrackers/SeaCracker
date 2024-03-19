b0d096c99f6c3da7c01abede7b69ac80fd5002f7
Cyprien <61788240+Cyprien-png@users.noreply.github.com>
2024-03-19 21:30:38 +0100

    fix: the spec file of host present question

--- src/app/host/host-present-question/host-present-question.component.spec.ts
+++ src/app/host/host-present-question/host-present-question.component.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { HostQuestionComponent } from './host-present-question.component';
+import { HostPresentQuestionComponent } from './host-present-question.component';
 
 describe('HostQuestionComponent', () => {
-  let component: HostQuestionComponent;
-  let fixture: ComponentFixture<HostQuestionComponent>;
+  let component: HostPresentQuestionComponent;
+  let fixture: ComponentFixture<HostPresentQuestionComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [HostQuestionComponent]
+      imports: [HostPresentQuestionComponent]
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(HostQuestionComponent);
+    fixture = TestBed.createComponent(HostPresentQuestionComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
