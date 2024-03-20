ee988f919bb15957b07d02af56d32f4a09693828
Cyprien Jaquier <61788240+Cyprien-png@users.noreply.github.com>
2024-03-20 10:56:17 +0100

    feat: add redirection to '/' when asks for a next step that doesn't exists

--- src/app/host-page/host-page.component.ts
+++ src/app/host-page/host-page.component.ts
@@ -1,26 +1,26 @@
-import {Component, OnInit, Type} from '@angular/core';
-import {RoomCommunicationsService} from "../services/communications/room-communications.service";
-import {RandomGenerator} from "../utils/random-generator";
-import {HostService} from "../services/game/host.service";
+import { Component, OnInit, Type } from '@angular/core';
+import { RoomCommunicationsService } from "../services/communications/room-communications.service";
+import { RandomGenerator } from "../utils/random-generator";
+import { HostService } from "../services/game/host.service";
 import { QrCodeComponent } from '../qr-code/qr-code.component';
 import {
   HostPresentQuestionComponent,
 } from "../host/host-present-question/host-present-question.component";
-import {HostPlayerlistComponent} from "../host/host-playerlist/host-playerlist.component";
-import {HostPodiumComponent} from "../host/host-podium/host-podium.component";
-import {PlayerListStep} from "../services/game/steps/player-list-step";
-import {PresentQuestionStep} from "../services/game/steps/present-question-step";
-import {AnswerStep} from "../services/game/steps/answer-step";
-import {LeaderboardStep} from "../services/game/steps/leaderboard-step";
-import {HostLeaderboardComponent} from "../host/host-leaderboard/host-leaderboard.component";
-import {PodiumStep} from "../services/game/steps/podium-step";
-import {HostAnswerComponent} from "../host/host-answer/host-answer.component";
-import {NgComponentOutlet} from "@angular/common";
-import {GameStep} from "../services/game/steps/game-step";
-import {HostComponent} from "../host/host-component";
-import {HostLoadingComponent} from "../host/host-loading/host-loading.component";
-import {QuizManagerService} from "../services/quizManager.service";
-import {ActivatedRoute, Router} from "@angular/router";
+import { HostPlayerlistComponent } from "../host/host-playerlist/host-playerlist.component";
+import { HostPodiumComponent } from "../host/host-podium/host-podium.component";
+import { PlayerListStep } from "../services/game/steps/player-list-step";
+import { PresentQuestionStep } from "../services/game/steps/present-question-step";
+import { AnswerStep } from "../services/game/steps/answer-step";
+import { LeaderboardStep } from "../services/game/steps/leaderboard-step";
+import { HostLeaderboardComponent } from "../host/host-leaderboard/host-leaderboard.component";
+import { PodiumStep } from "../services/game/steps/podium-step";
+import { HostAnswerComponent } from "../host/host-answer/host-answer.component";
+import { NgComponentOutlet } from "@angular/common";
+import { GameStep } from "../services/game/steps/game-step";
+import { HostComponent } from "../host/host-component";
+import { HostLoadingComponent } from "../host/host-loading/host-loading.component";
+import { QuizManagerService } from "../services/quizManager.service";
+import { ActivatedRoute, Router } from "@angular/router";
 @Component({
   selector: 'app-host-page',
   standalone: true,
@@ -28,20 +28,26 @@ import {ActivatedRoute, Router} from "@angular/router";
   templateUrl: './host-page.component.html',
   styleUrl: './host-page.component.scss'
 })
-export class HostPageComponent{
-  constructor(public host:HostService, private quizManager : QuizManagerService, private activatedroute: ActivatedRoute, private router : Router) {
+export class HostPageComponent {
+  constructor(public host: HostService, private quizManager: QuizManagerService, private activatedroute: ActivatedRoute, private router: Router) {
     const quizId = parseInt(this.activatedroute.snapshot.paramMap.get("id") ?? "-1")
     const quiz = this.quizManager.getQuizById(quizId);
-    if(!quiz)
+    if (!quiz)
       this.router.navigate(['/']);
     this.host.setupQuiz(quiz!)
+  }
+
+  handleNextStepClick(): void {
+    if (this.host.getCurrentStep()()!.hasNextStep())
+      return this.host.nextStep();
+    this.router.navigate(['/']);
   }
 
   /**
    * For now, we simply return one component per step. 1:1 mapping.
    */
-  get activeStepComponent() : Type<HostComponent>  {
-    if(this.host.getCurrentQuestion()() == undefined)
+  get activeStepComponent(): Type<HostComponent> {
+    if (this.host.getCurrentQuestion()() == undefined)
       return HostLoadingComponent;
     switch (this.host.getCurrentStep()()!.constructor) {
       case PlayerListStep:
