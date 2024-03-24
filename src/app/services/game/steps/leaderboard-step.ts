e1b703d47367613cd11a7c6734c2bbdb95f504ac
eliott <eliott.jaquier@gmail.com>
2024-03-24 10:40:06 +0100

    feat : new reveal step models

--- src/app/services/game/steps/leaderboard-step.ts
+++ src/app/services/game/steps/leaderboard-step.ts
@@ -4,7 +4,7 @@ import {PresentQuestionStep} from "./present-question-step";
 import {PodiumStep} from "./podium-step";
 
 /**
- * A leaderboard phase in the host is a dump phase (ment to display score based on game status)
+ * A leaderboard phase in the host is a simple phase ment to display score based on game status
  */
 export class LeaderboardStep extends GameStep{
   goToNextStep(): GameStep {
